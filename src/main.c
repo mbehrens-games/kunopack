333982ca485b1e97e1a7bfd8f718693b79b48096
Michael Behrens <102385787+mbehrens-games@users.noreply.github.com>
2026-02-21 12:02:58 -0800

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 #include "art.h"
+#include "con.h"
 #include "rom.h"
 
 /******************************************************************************/
@@ -15,12 +16,17 @@ int main(int argc, char *argv[])
 {
   rom_format();
 
+  /* parse con file */
+  con_load_file("test_spriteset.con");
+
+#if 0
   /* create test sprite set */
   art_clear_rom_data_vars();
 
   art_load_gif("mai_standing.gif");
 
   art_add_files_to_rom();
+#endif
 
   /* save the rom! */
   rom_save("test.kn1");
